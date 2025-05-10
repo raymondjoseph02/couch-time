2644cf6e85c8f2cde4cc7fa6a286d0e16a9a0e4e
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-05-10 13:26:10 +0100

    fix:fix narbav background image position

--- src/app/components/dashboard/NavBar.tsx
+++ src/app/components/dashboard/NavBar.tsx
@@ -47,8 +47,11 @@ export const NavBar = () => {
       style={{
         backgroundImage: `url(${couch_bg.src})`,
         backgroundSize: "14rem",
+        // backgroundPosition: "20px",
+        backgroundPositionX: "calc(100% - 30px)",
+        backgroundPositionY: "0px",
       }}
-      className="space-y-8 bg-no-repeat bg-top-right 2 fixed  pb-5 z-100 w-[calc(100vw-100px)] bg-[#030306] left-25 pl-5 top-0 pt-16"
+      className="space-y-8 bg-no-repeat  fixed  pb-5 z-100 w-[calc(100vw-100px)] bg-[#030306] left-25 pl-5 top-0 pt-16"
     >
       <div>
         <h1 className="text-4xl font-semibold text-white">My cinema</h1>
@@ -62,9 +65,9 @@ export const NavBar = () => {
               key={index}
               role="tab"
               onClick={tab.action}
-              className="px-5 py-2 relative cursor-pointer"
+              className="relative px-5 py-2 cursor-pointer"
             >
-              <span className="relative z-20  text-white"> {tab.tabName}</span>
+              <span className="relative z-20 text-white"> {tab.tabName}</span>
               {isActive && (
                 <motion.div
                   layout="preserve-aspect"
